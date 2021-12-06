4b13ad6d36e26293f0ed042e151e8408173d65bd
aerlevsedi <sobasmarta@gmail.com>
2021-12-06 11:34:49 +0100

    Task6

---
+++ AoC2021/task6_lanternfish/task6_A_lanternfish.py
@@ -0,0 +1,31 @@
+file = open("task6_input.txt")
+line = file.readline()
+
+numbers = line.split(",")
+
+fishes = [0 for i in range(9)]
+
+for i in range(len(numbers)) :
+    timer = int(numbers[i])
+    fishes[timer] += 1
+
+print(fishes)
+
+for d in range(80) :
+    print("przed", fishes)
+    tmp = fishes[0]
+
+    for i in range(8) :
+        fishes[i] = fishes[i+1]
+
+    fishes[6] += tmp
+    fishes[8] = tmp
+
+    print("po", fishes, "\n")
+
+sum = 0
+
+for count in fishes :
+    sum += count
+
+print(sum)
